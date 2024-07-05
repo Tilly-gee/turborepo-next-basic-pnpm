8eccbc09e6b016e7c9098101c10ed75819a22440
Tilly-gee <104358587+Tilly-gee@users.noreply.github.com>
2024-07-05 08:06:52 +0100

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             </code>
           </li>
           <li>Save and see your changes instantly.</li>
-          <li>Save and see your changes instantly, try again.</li>
+          <li>Save and see your changes instantly, try again. One more time</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
